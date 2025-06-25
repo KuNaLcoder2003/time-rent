7a833ee339579387ff4d5bf9dd8c226535409569
Kunal Singh <kunalindia59@gmail.com>
2025-06-25 14:32:34 +0530

    adding booking router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import user_router from "./user"
+import booking_router from "./booking"
 
 const router = express.Router()
 
 router.use('/user' , user_router)
+router.use('/booking' , booking_router)
 
 
 
