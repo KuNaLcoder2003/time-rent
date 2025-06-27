f2ffcc6e155aa1c74195d36cf44b2dd4c13a9944
Kunal Singh <kunalindia59@gmail.com>
2025-06-27 15:23:51 +0530

    changes in routes

--- backend/src/routes/booking.ts
+++ backend/src/routes/booking.ts
@@ -5,31 +5,26 @@ import Stripe from "stripe"
 import authMiddleware from "../middlewares/authMiddleWare"
 import dotenv from "dotenv"
 
+
 dotenv.config()
 
 const stripe_api_secret = process.env.STRIPE_SECRET
 
 const stripe = new Stripe(`${stripe_api_secret}`)
 
-
-
-
 const prisma = new PrismaClient()
 
 const booking_router = express.Router()
 
-
-
 booking_router.post('/create-payment', async (req: express.Request, res: express.Response) => {
-    const { amount, timeSlots } = req.body
-    const user_email = "kunalindia59@gmail.com"
+    const { amount, timeSlots ,email , user_email } = req.body
 
     try {
 
         const session = await stripe.checkout.sessions.create({
             mode: "payment",
             success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/",
+            cancel_url: "http://localhost:5173/cancel",
             line_items: [
                 {
                     price_data: {
@@ -43,9 +38,24 @@ booking_router.post('/create-payment', async (req: express.Request, res: express
                 }
             ]
         })
-        console.log(session.id)
+        const new_booking = await prisma.booking.create({
+            data: {
+                from_user_name: 'Lappu',
+                request_user_email: email,
+                created_at: new Date(),
+                scheduled_at: new Date(),
+                meet_url: "",
+                expired: false,
+                timeSlotId: timeSlots?.id,
+                to_user: user_email,
+                date: new Date(),
+                payment : false
+            }
+        })
+       
         res.status(200).json({
-            sessionId: session.id
+            sessionId: session.id,
+            bookingId : new_booking.id
         })
     } catch (error) {
         console.log(error)
@@ -55,35 +65,28 @@ booking_router.post('/create-payment', async (req: express.Request, res: express
     }
 })
 
-booking_router.post('/make-booking', async (req: express.Request, res: express.Response) => {
-    const { email, timeSlot, to_email } = req.body.email;
+booking_router.put('/make-booking', async (req: express.Request, res: express.Response) => {
+    const {  bookingId } = req.body;
     try {
-        if (!email) {
-            res.status(400).json({
-                message: 'Bad request'
-            })
-            return
-        }
-
-        const new_booking = prisma.booking.create({
-            data: {
-                from_user_name: 'Lappu',
-                request_user_email: email,
-                created_at: new Date(),
-                scheduled_at: new Date(),
-                meet_url: "",
-                expired: false,
-                timeSlotId: timeSlot?.id,
-                to_user: to_email,
-                date: new Date()
+        
+            
+        const updated_booking  = await prisma.booking.update({
+            where : {id : bookingId},
+            data : {
+                payment :  true
             }
         })
-        if (!new_booking) {
+
+        if(!updated_booking) {
             res.status(402).json({
-                message: 'Not able to create booking at the moment'
+                message : 'Not able to book the slot , will initiate refund'
             })
             return
         }
+        res.status(200).json({
+            message : 'Booking confirmed',
+            booking : updated_booking
+        })
 
     } catch (error) {
         res.status(500).json({
@@ -92,11 +95,14 @@ booking_router.post('/make-booking', async (req: express.Request, res: express.R
     }
 })
 
-booking_router.get('/details', async (req: express.Request, res: express.Response) => {
+booking_router.get('/details/:email', async (req: express.Request, res: express.Response) => {
+
+    const email  = req.params.email
+
 
     try {
         const user = await prisma.user.findFirst({
-            where: { email: "kunalindia59@gmail.com" }
+            where: { email: email }
         })
 
         if (!user) {
