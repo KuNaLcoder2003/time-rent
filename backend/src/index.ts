6b52f7d174f3f0ad7e0c5111f0274645c1449820
Kunal Singh <kunalindia59@gmail.com>
2025-06-29 16:58:25 +0530

    configuring dot env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import router from "./routes"
 import dotenv from "dotenv"
+dotenv.config()
 
 const PORT = process.env.PORT || 3000
 const app = express()
