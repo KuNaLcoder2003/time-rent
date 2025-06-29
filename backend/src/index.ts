6f9aaaf2a3fe3e8b7b42d04ac27674b2382b4dfb
Kunal Singh <kunalindia59@gmail.com>
2025-06-29 16:52:02 +0530

    adding ENV PORT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes"
+import dotenv from "dotenv"
 
-
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(cors())
@@ -11,6 +12,6 @@ app.use(express.json())
 app.use('/api/v1' , router)
 
 
-app.listen(3000 , ()=>{
+app.listen(PORT , ()=>{
     console.log('App started')
 })
